b6abf629b6787548f5fd8efe3b8d978c094910f5
andreigheorghiu-toast <andrei.gheorghiu@toasttab.com>
2023-03-02 10:01:55 +0000

    fixed types declarations

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,4 @@ import "./style.css";
 import App from "./App.vue";
 import VueResponsiveness from "./types/vue-responsiveness";
 
-createApp(App)
-  .use(VueResponsiveness, {
-    xs: 0,
-    sm: 576,
-    md: 768,
-    lg: 992,
-    xl: 1200,
-    "2xl": 1500,
-    "3xl": 1800,
-  })
-  .mount("#app");
+createApp(App).use(VueResponsiveness).mount("#app");
