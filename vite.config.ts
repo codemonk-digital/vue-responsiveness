cc1e01e61baa8cf862432b9dff1462172d9bff5d
andreigheorghiu-toast <andrei.gheorghiu@toasttab.com>
2023-03-03 10:16:42 +0000

    testing wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "**/src/*", "**/public/*"],
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
