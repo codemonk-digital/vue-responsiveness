abaf3cbe0c1872488acefc2332de59c05db2386e
Andrei Gheorghiu <arhicub@gmail.com>
2023-03-03 12:03:36 +0000

    Added tests (#3)
    * added presets; fixed types; fixed build; updated readme
    
    * testing wip
    
    * added tests
    
    * updated lock
    
    ---------
    
    Co-authored-by: andreigheorghiu-toast <andrei.gheorghiu@toasttab.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
+import { configDefaults } from "vitest/config";
 import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 export default defineConfig({
   plugins: [vue()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    exclude: [...configDefaults.exclude, "**/src/*", "**/public/*"],
+    setupFiles: [resolve(__dirname, "test/setup.ts")],
+    reporters: "dot",
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
   build: {
     lib: {
       entry: resolve(__dirname, "lib/index.ts"),
