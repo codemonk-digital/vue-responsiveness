21584af5abba11d6cf0cd523503d673af506b3e3
amdrei-gheorghiu <arhicub@gmail.com>
2023-03-12 19:46:39 +0000

    removed SSR guards on

--- lib/vue-responsiveness.ts
+++ lib/vue-responsiveness.ts
@@ -13,39 +13,39 @@ export const VueResponsiveness = {
     app: App,
     breakpoints: VueResponsivenessBreakpoints = Presets.Bootstrap_5
   ): void {
-    if (typeof window !== 'undefined') {
-      const intervals: Record<string, { min: string; max: string }> =
-        Object.entries(breakpoints)
-          .sort(([, a], [, b]) => (a || 0) - (b || 0))
-          .reduce((out, [key, min], i, arr) => {
-            out[key] = {
-              min: min ? `(min-width: ${min}px)` : '',
-              max: arr[i + 1]?.[1]
-                ? `(max-width: ${(arr[i + 1][1] as number) - 0.1}px)`
-                : ''
-            }
-            return out
-          }, {} as Record<string, { min: string; max: string }>)
-      matches = reactive({
-        isMin: computed(() => (key: string) => matches[key]?.min || false),
-        isMax: computed(() => (key: string) => matches[key]?.max || false),
-        isOnly: computed(() => (key: string) => matches[key]?.only || false),
-        ...Object.assign(
-          {},
-          ...Object.keys(intervals).map((_) => ({
-            [_]: { min: false, max: false, only: false }
-          })),
-          {
-            current: computed(
-              () =>
-                Object.entries(matches).find(
-                  ([, value]) => typeof value === 'object' && value.only
-                )?.[0] as string
-            )
+    const intervals: Record<string, { min: string; max: string }> =
+      Object.entries(breakpoints)
+        .sort(([, a], [, b]) => (a || 0) - (b || 0))
+        .reduce((out, [key, min], i, arr) => {
+          out[key] = {
+            min: min ? `(min-width: ${min}px)` : '',
+            max: arr[i + 1]?.[1]
+              ? `(max-width: ${(arr[i + 1][1] as number) - 0.1}px)`
+              : ''
           }
-        )
-      })
+          return out
+        }, {} as Record<string, { min: string; max: string }>)
+    matches = reactive({
+      isMin: computed(() => (key: string) => matches[key]?.min || false),
+      isMax: computed(() => (key: string) => matches[key]?.max || false),
+      isOnly: computed(() => (key: string) => matches[key]?.only || false),
+      ...Object.assign(
+        {},
+        ...Object.keys(intervals).map((_) => ({
+          [_]: { min: false, max: false, only: false }
+        })),
+        {
+          current: computed(
+            () =>
+              Object.entries(matches).find(
+                ([, value]) => typeof value === 'object' && value.only
+              )?.[0] as string
+          )
+        }
+      )
+    })
 
+    if (typeof window !== 'undefined') {
       Object.entries(intervals).forEach(([interval, mediaQueries]) => {
         const queryLists: Record<'min' | 'max', MediaQueryList> = {
           min: window.matchMedia(mediaQueries.min),
@@ -63,9 +63,9 @@ export const VueResponsiveness = {
           listener(mediaQueryList)
         })
       })
-
-      app.config.globalProperties.$matches = matches
     }
+
+    app.config.globalProperties.$matches = matches
   }
 }
 
